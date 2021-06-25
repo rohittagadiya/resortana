a8bd3de25dcf8936748c2d4d39921ba10b90f75c
rohittagadiya <61516471+rohittagadiya@users.noreply.github.com>
2021-06-25 12:23:51 +0530

    1. added home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,22 +24,6 @@ export class HomePage implements OnInit {
       icon: './assets/images/services.png',
       title: 'Services'
     },
-    {
-      icon: './assets/images/resort_villas.png',
-      title: 'Resort & Villas'
-    },
-    {
-      icon: './assets/images/wedding_hall.png',
-      title: 'Wedding Hall'
-    },
-    {
-      icon: './assets/images/camp.png',
-      title: 'Camp'
-    },
-    {
-      icon: './assets/images/services.png',
-      title: 'Services'
-    },
   ];
 
   constructor() { }
